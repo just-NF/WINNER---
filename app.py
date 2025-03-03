d622e0a832c76f02dc909433a0f5b07bba28af24
just-NF <ajokedi.aane@gmail.com>
2025-03-03 21:00:16 +0100

    added logic for submitting

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
+import mysql.connector
 
 app = Flask(__name__)
+
+
+@app.route("/details", methods = ["POST", "GET"])
+def details ():
+    user_name = request.form['user_name']
+    phone_number = request.form['phone_number']
+    number_of_items = request.form['number_of_items']
+    total_amount = request.form['total_amount']
+    order_date = request.form['order_date']
+    # connect to the data base
+    mydb = mysql.connector.connect(
+        host="sql8.freesqldatabase.com",
+        user = "sql8764909",
+        password = "8cEtjHmsu5",
+        database = "sql8764909"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute('INSERT INTO CustomerDetails (user_name, phone_number, number_of_items, total_amount, order_date) VALUES (%s, %s, %s, %s, %s )', (user_name, phone_number, number_of_items, total_amount, order_date))
+    mydb.commit()
+    return render_template('index.html')
+
 
 @app.route("/")
 def index():
